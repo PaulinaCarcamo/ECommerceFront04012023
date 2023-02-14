dbacd67e664f5893c11266b23ea7c5a3949453a2
PaulinaCarcamo <108833481+PaulinaCarcamo@users.noreply.github.com>
2023-02-14 11:56:47 -0300

    Confirmation modal styling changes, reset cart

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,21 +3,12 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-
-import { resetCart } from '../redux/cartRedux';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+import { resetCart } from '../redux/cartRedux.js';
+import './modal.css'
+import { Check, Clear } from '@mui/icons-material';
 
 export default function ConfirmationModal() {
   const [open, setOpen] = React.useState(false);
@@ -27,23 +18,28 @@ export default function ConfirmationModal() {
 
   return (
     <div>
-      <Button onClick={handleOpen}>RESET CART</Button>
+      <Link onClick={handleOpen} classname='link'>RESET CART</Link>
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-          <Button onClick={()=> dispatch(resetCart())}>YES</Button>
-          {/* <Link onClick={() => dispatch(resetCart())}>RESET CART</Link> */}
-          <Button onClick={() => {handleClose()}}>NO</Button>
+        <Box className='box'>
+          <div>
+
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Are you sure?
+            </Typography>
+            {/* <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Click on the buttons to clear cart.
+          </Typography> */}
+          </div>
+          <div>
+            <Button onClick={() => dispatch(resetCart())}>YES <Check /></Button>
+            {/* <Link onClick={() => dispatch(resetCart())}>RESET CART</Link> */}
+            <Button onClick={() => { handleClose() }}>NO <Clear /></Button>
+          </div>
         </Box>
       </Modal>
     </div>
