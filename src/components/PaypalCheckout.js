113c279c990201222ae05ab88de02ae83d5d655f
PaulinaCarcamo <108833481+PaulinaCarcamo@users.noreply.github.com>
2023-02-17 12:53:04 -0300

    Modal checkout added, testing order details msg

--- src/components/PaypalCheckout.js
+++ src/components/PaypalCheckout.js
@@ -1,8 +1,13 @@
-import Snackbar from '@mui/material/Snackbar';
-import IconButton from '@mui/material/IconButton';
-import { Close } from '@mui/icons-material';
 import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js';
 import { useState } from 'react';
+import { CheckCircleOutlineOutlined, Home, ShoppingBag, ShoppingCart, VpnKey } from '@mui/icons-material';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { Link } from 'react-router-dom';
+
+// import { resetCart } from '../redux/cartRedux.js';
+// import { useDispatch } from 'react-redux';
 // import { useNavigate } from 'react-router-dom';
 import './paypalCheckout.css';
 
@@ -11,9 +16,16 @@ const PaypalCheckout = (props) => {
     const { product } = props
     const [paidFor, setPaidFor] = useState(false)
     const [error, setError] = useState(null)
-    // const navigate = useNavigate()
-
     const [open, setOpen] = useState(false);
+    const [data, setData] = useState()
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    // const navigate = useNavigate()
+    // const dispatch = useDispatch();
+
+    // const [open, setOpen] = useState(false);
 
     // CALL BACKEND FUNCTION TO FULFILL ORDER
     // const handleApprove = (orderID) => {
@@ -23,8 +35,6 @@ const PaypalCheckout = (props) => {
     // if (paidFor) {
     //     navigate("/success", { replace: true })
     //         }
-
-    const [data, setData] = useState()
 
     const onCreateOrder = (data, actions) => {
         return actions.order.create({
@@ -40,7 +50,6 @@ const PaypalCheckout = (props) => {
 
     const onApproveOrder = (data, actions) => {
         return actions.order.capture().then((details) => {
-            // const name = details.payer.name.given_name;
             const payer = details.payer.name.given_name;
             const surname = details.payer.name.surname;
             const status = details.status;
@@ -49,18 +58,17 @@ const PaypalCheckout = (props) => {
             const purchase_units = JSON.parse(purchase);
             console.log(details);
 
-            alert(
-                "Thank you for your purchase " + `${payer}` + " " + `${surname}` + " || "
-                + "Your order has been: " + `${status}` + " || "
-                + "This is your id purchase: " + " " + `${orderid}` + " || "
-                + "TOTAL PRICE: " + " $ " + `${purchase_units[0].amount.value}` + " || "
-                + "SENDING PRODUCTS TO: " + `${purchase_units[0].shipping.address.address_line_1}` + " || "
-                + "REGION: " + `${purchase_units[0].shipping.address.admin_area_2}`
-            );
-
-            openSnackbar();
+            handleOpen();
             // setData(purchase_units[0].shipping.name.full_name)
-            setData(payer + " " + surname + " " + status)
+            // setData(details)
+            setData(
+                "Thank you for your purchase " + payer + " " + surname + ". "
+                + "Your order has been " + status + ". "
+                + "Order ID: " + orderid + ". "
+                + "Total price: $" + purchase_units[0].amount.value + ". "
+                + "Address: " + purchase_units[0].shipping.address.address_line_1 + ", "
+                + purchase_units[0].shipping.address.admin_area_2
+            )
             console.log(data);
         });
     }
@@ -68,21 +76,11 @@ const PaypalCheckout = (props) => {
     if (error)
         alert(error)
 
-    const openSnackbar = () => {
-        setOpen(true);
-    };
-
-    const closeSnackbar = (event, reason) => {
-        if ("clickaway" === reason) return;
-        setOpen(false);
-    };
-
     return (
         <PayPalScriptProvider options={{
             "client-id": 'Aa7XBjPY3TRPCsKfu_Wzam9g06BfA2IfqR6WtAtvXfHtgyMvA-WDyzzzCqqXpplGs-JHV6dq8kegaq9J',
             //DISABLE CREDIT CARD PAYMENT OPTION: 'disable-funding': 'card'
-        }}
-        >
+        }}>
             <PayPalButtons
                 // style={{
                 //     layout: 'horizontal',
@@ -93,40 +91,49 @@ const PaypalCheckout = (props) => {
                 onError={(err) => {
                     setError(err)
                     console.log("Paypal checkout onError", err);
-                }}
-            />
+                }} />
 
             {!paidFor &&
-
-
-                    <Snackbar
-                        anchorOrigin={{
-                            horizontal: "left",
-                            vertical: "bottom",
-                        }}
-                        ContentProps={{
-                            sx: {
-                                background: "purple"
-                            }
-                        }}
+                <div>
+                    {/* <Link onClick={handleOpen} classname='link'>RESET CART</Link> */}
+                    <Modal
                         open={open}
-                        // autoHideDuration={5000}
-                        message={("PURCHASE SUCCESSFULLY COMPLETED", `${data}`)}
-                        onClose={closeSnackbar}
-                        action={
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box className='box'>
                             <div>
-                                <IconButton
-                                    size="small"
-                                    aria-label="close"
-                                    color="inherit"
-                                    onClick={closeSnackbar}
-                                >
-                                    <Close fontSize="small" />
-                                </IconButton>
-                            </div>
-                        }
-                    />
 
+                                <Typography id="modal-modal-title" variant="h6" component="h2">
+                                    DONE <CheckCircleOutlineOutlined style={{ color: "green" }} />
+                                </Typography>
+                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                    {data}
+                                </Typography>
+                            </div>
+                            <div className='checkout-icons'>
+
+                                <Link to='/' className='checkout-icon'>
+                                    <Home />
+                                    Home
+                                </Link>
+                                <Link to='/allproducts' className='checkout-icon' >
+                                    <ShoppingBag />
+                                    Shop
+                                </Link>
+                                <Link onClick={handleClose} className='checkout-icon'>
+                                    <ShoppingCart />
+                                    Cart
+                                </Link>
+                                <Link to='/' className='checkout-icon'>
+                                    <VpnKey />
+                                    Login
+                                </Link>
+                            </div>
+                        </Box>
+                    </Modal>
+                </div>
             }
         </PayPalScriptProvider>
     )
